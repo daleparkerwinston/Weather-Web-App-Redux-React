e5acae6f9555c3dd55afa194b04e44da50c6672e
daleparkerwinston <daleparkerwinston@gmail.com>
2017-03-16 18:18:03 -0400

    Added Google Maps functionality.

---
+++ src/components/google_map.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+class GoogleMap extends Component {
+  componentDidMount() {
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: {
+        lat: this.props.lat,
+        lng: this.props.lng
+      }
+    })
+  }
+
+  render() {
+    // creates a reference to the div called map
+    // as this.refs.map
+    return (
+      <div ref="map"/>
+    );
+  }
+}
+
+export default GoogleMap;
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,18 +2,20 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
   renderWeather(cityData) {
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp * 9/5 - 459.67);
-    const winds = cityData.list.map(weather => weather.wind.speed * (3600/1609.344));
+    const temps = cityData.list.map(weather => weather.main.temp * 9 / 5 - 459.67);
+    const winds = cityData.list.map(weather => weather.wind.speed * (3600 / 1609.344));
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const {lon, lat} = cityData.city.coord;
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lng={lon} lat={lat}/>{name}</td>
         <td><Chart data={temps} color="green" units="&#8457;"/></td>
         <td><Chart data={winds} color="black" units="MPH"/></td>
         <td><Chart data={humidities} color="blue" units="%"/></td>
